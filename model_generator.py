b5a6db2750ab1d7f0e45b176b7204c30358e9591
allenh <hope14@gmail.com>
2017-05-06 17:15:24 -0700

    initial implementation of generator version

---
+++ model_generator.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  6 15:19:06 2017
+
+@author: hope
+"""
+
+import csv
+import cv2
+import numpy as np
+
+dpath = '/Users/hope/Documents/python/carND/CarND-Behavioral-Cloning-P3/data'
+
+lines = []
+with open(dpath + '/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    csvHeader = next(reader)
+    for line in reader:
+        lines.append(line)
+
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+
+import sklearn
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    
+    while 1: # Loop forever so the generator never terminates
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = dpath + '/IMG/' + batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+model = Sequential()
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+history_object=model.fit_generator(train_generator, 
+                                   samples_per_epoch=len(train_samples), 
+                                   validation_data=validation_generator, 
+                                   nb_val_samples=len(validation_samples), 
+                                   nb_epoch=5)
+
+import matplotlib.pyplot as plt
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
+
+model.save('model.h5')
